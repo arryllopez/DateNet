fbf5630bff923e34b0a445f92090ea0a9c751c7f
ISHAN BANGA <ishan.banga0@gmail.com>
2024-11-23 17:55:47 -0500

    Changed Profile.html Styling

--- Views/Js/Controllers/authController.js
+++ Views/Js/Controllers/authController.js
@@ -154,7 +154,7 @@ async function getUserProfile(req, res) {
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-
+        
         res.status(200).json(user); 
     } catch (error) {
         console.error('Error fetching profile:', error);
